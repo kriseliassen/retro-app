379bc154424e19d06d9bb225a15259d6fcf14384
jordiaixela <jordiaixela@gmail.com>
2021-11-26 12:48:21 +0100

    Skeleton works with React!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState, useEffect } from 'react'
 
 const App = ()=> {
+  const [teams, setTeams] = useState([])
+  const data = async () => {
+    let resp = await fetch('/db/teams')
+    const teams = await resp.json()
+    console.log(teams)
+    setTeams(teams)
+  }
+useEffect(()=>{data()},[])
   return (
     <div className="App">
       <p>Retro App: final project</p>
-      
+      {teams?.map(i=>(<p>{i.name}</p>))}
     </div>
   );
 }
