3fb0e37ebf04718d9e01c1390cb35fc2b474697b
jordiaixela <jordiaixela@gmail.com>
2021-11-26 13:34:26 +0100

    MR

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -13,23 +13,4 @@ pool.connect();
 module.exports = {
   query: async (text, params) => pool.query(text, params),
   GET_ALL_TEAMS: 'SELECT * FROM teams',
-//   GET_BY_ID: 'SELECT * FROM products WHERE id = $1',
-//   GET_ALL_PRODUCT_GROUPS: 'SELECT * FROM productgroups',
-//   ADD_PRODUCT: `
-//   INSERT INTO Products (name, description, price, ProductGroupId)
-//   VALUES ($1, $2, $3, $4)
-//   `,
-//   UPDATE_BY_ID: `
-//   UPDATE Products
-//   SET name = $1, description = $2, price = $3, ProductGroupId = $4
-//   WHERE id = $5
-//   `,
-//   DELETE_BY_ID: 'DELETE FROM products WHERE id = $1',
-//   GETT_BY_GROUP_NAME: `
-//   SELECT * FROM productgroups
-//   INNER JOIN products
-//   ON
-//   productgroups.id = products.productgroupid
-//   where lower(productgroups.name) = $1
-//   `,
 };
--- server/src/db/functions.js
+++ server/src/db/functions.js
@@ -1,10 +1,8 @@
 const db =  require('./db.js');
 
 module.exports ={
-
     teams: async () => {
         const { rows } = await db.query(db.GET_ALL_TEAMS);
-        // try to break all sql out to separate .sql files
         return rows;
     } 
 }
