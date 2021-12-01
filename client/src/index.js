f1c9ebe34bbeb549b60e0fe3168b9efb50c5cca7
Kristin Eliassen <kristin.eliassen@gmail.com>
2021-12-01 16:21:12 +0100

    Testing github desktop

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
