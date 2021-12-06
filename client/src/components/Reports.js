63f9abcf13917ccedf5e68e45eac2462e9cc726f
jordiaixela <jordiaixela@gmail.com>
2021-12-06 14:00:52 +0100

    report gets data in object, but does not render correctly.

--- client/src/components/Reports.js
+++ client/src/components/Reports.js
@@ -10,6 +10,8 @@ const Reports = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [entries, setEntries] = useState([])
+    const [output, setOutput] = useState([])
+
     const getEntries = async (token) =>{
         const resp = await fetch(`${SERVER_URL}/db/reports`, {
             method: 'POST',
@@ -26,26 +28,64 @@ const Reports = () => {
           console.log(rawEntries)
           setEntries(rawEntries.reports)
     }
+    const allIndividualTeamEntries = () => {
+        const data = [...entries]
+        const differentEntries = []
+        const questionsSets = []
+        data.forEach(i=> {
+            if (differentEntries.includes(i.entries_id)){
+                return
+            } else {
+                differentEntries.push(i.entries_id)
+            }
+        })
+        differentEntries.forEach(i => {
+            let arr = []
+            data.filter(item=>item.entries_id === i)
+            .forEach(j=> {
+                if (arr.includes(j.questions_id)){
+                    return
+                } else {
+                    arr.push(j.questions_id)
+                }
+            })
+            questionsSets.push(arr)
+        })
+        let filteredData = []
+        differentEntries.forEach((entry, i)=>{
+            let intermediate = []
+            questionsSets[i].forEach(q=> {
+                intermediate.push(data.find(item=> item.entries_id === entry && item.questions_id === q ))
+            })
+            filteredData.push({entry: entry, questions:intermediate})
+        })
+        
+    return (filteredData)
+    }
     useEffect(() => {
         const token = localStorage.getItem('retroToken');
         if(!token) {
-          navigate('/login')
-          return;
+            navigate('/login')
+            return;
         } else if (!user.user) {
-          dispatch(fetchUser(token))
+            dispatch(fetchUser(token))
         } 
         getEntries(token);
-      }, [user]);
+        setOutput(allIndividualTeamEntries())
+    }, [user]);
+    
+    
+    console.log('FUNCTION OUTPUT',allIndividualTeamEntries());
 
     return (
         <div>
 
-            <p> REPORTS: Number of entries: {entries?.length}</p>
-            {entries.map(item=>(
+            <p> REPORTS: Number of entries: {output?.length}</p>
+            {output?.map(item=>(
                 <div>
-                    <p>Entry Id: {item.entries_id}</p>
-                    <p>Question: {item.question}</p>
-                    <p>Response: {item.response}</p>
+                    <p>Entry Id: {item.questions.entries_id}</p>
+                    <p>Question: {item.questions.question}</p>
+                    <p>Response: {item.questions.response}</p>
                 </div>    
                 ))
             }
