7998dbb8bc34df4a38345b9a8e2955d95ddd1448
jordiaixela <jordiaixela@gmail.com>
2021-12-06 16:27:42 +0100

    Join SQL works!!!

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -96,19 +96,20 @@ module.exports = {
     )
   `,
   GET_ALL_ENTRIES: `
-  SELECT entries_id, date, user_id, users.team_id, 
+  select entries_id, date, user_id, users.team_id, 
   entries.templates_id, name AS template_name, 
   questions_id, question, text AS response, type, responses.id as responseId, 
   first_name, last_name
-  from entries 
-  JOIN templates
-  ON templates.id = entries.templates_id  
-  JOIN questions
-  ON questions.templates_id = templates.id 
-  JOIN responses
-  ON responses.questions_id = questions.id
-  JOIN users
+from entries
+join templates
+on entries.templates_id = templates.id
+left JOIN questions
+ON questions.templates_id = entries.templates_id 
+JOIN responses
+ON responses.entries_id = entries.id and responses.questions_id = questions.id
+JOIN users
   ON users.id = entries.user_id
+
   WHERE users.team_id = $1
   `,
 };
--- server/src/db/dbSeed/joinquery-entriesdata.sql
+++ server/src/db/dbSeed/joinquery-entriesdata.sql
@@ -1,12 +1,15 @@
 -- this query outputs a table with all data for each entry
-SELECT entries_id, date, user_id, users.team_id, entries.templates_id, name AS template_name, questions_id, question, text AS response, type
-from entries
-JOIN templates
-ON templates.id = entries.templates_id
-LEFT JOIN questions
-ON questions.templates_id = templates.id
-LEFT JOIN responses
-ON responses.questions_id = questions.id
-LEFT JOIN users
-ON users.id = entries.user_id
+SELECT entries_id, date, user_id, users.team_id, 
+  entries.templates_id, name AS template_name, 
+  questions_id, question, text AS response, type, responses.id as responseId, 
+  first_name, last_name
+  from entries 
+  JOIN templates
+  ON templates.id = entries.templates_id  
+  JOIN questions
+  ON questions.templates_id = templates.id 
+  JOIN responses
+  ON responses.questions_id = questions.id
+  JOIN users
+  ON users.id = entries.user_id
 
