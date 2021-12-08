e7d5e996742df3efb9c2153f852dbbba87ac732a
jordiaixela <jordiaixela@gmail.com>
2021-12-08 13:16:09 +0100

    MR

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,7 +12,11 @@ const About = () => {
         About Retro
       </h1>
       <p className="About__text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      An app that helps teams log their retrospectives and track their progress. 
+      <br/>
+      <br/>
+      Users can create or join teams, use predefined retrospectives models or create their own, and track their team evolution.
+
       </p>
       <ul className="About__list">
         <p className="About__created-by">Created by
